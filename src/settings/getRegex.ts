02fdacd9926e8d85b2fd54ff869dc207e9429c02
Nicolas Descamps <nicolas.descamps@theodo.com>
2024-09-17 15:08:04 +0200

    Update regex

--- src/settings/getRegex.ts
+++ src/settings/getRegex.ts
@@ -2,7 +2,7 @@ import * as vscode from "vscode";
 
 export const getRegex = () => {
   const defaultRegex =
-    /^(yarn test|yarn jest|npm run test|npm run jest|npm test|npm jest|jest|test)/;
+    /^(yarn test|yarn jest|npm run test|npm run jest|npm test|npm jest|jest|test|npm exec jest)/;
 
   const regex = (() => {
     try {
