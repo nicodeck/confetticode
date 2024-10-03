686cb5316232c9d05e18c856ab6e4e1a324c9b8f
Nicolas Descamps <nicolas.descamps@theodo.com>
2024-10-03 12:37:06 +0200

    Turn custom regex setting into a list of regex

--- src/settings/getRegex.ts
+++ src/settings/getRegex.ts
@@ -2,21 +2,29 @@ import * as vscode from "vscode";
 
 export const getRegex = () => {
   const defaultRegex =
-    /^(yarn test|yarn jest|npm run test|npm run jest|npm test|npm jest|jest|test|npm exec jest)/;
+    /^(yarn test|yarn jest|npm run test|npm run jest|npm test|npm jest|jest|test)/;
 
   const regex = (() => {
     try {
-      const configurationRegex = new RegExp(
-        vscode.workspace
-          .getConfiguration("confetticode")
-          .get("customCommandRegex") as string
+      const configurationRegexStringList = vscode.workspace
+        .getConfiguration("confetticode")
+        .get("customCommandRegexList") as string[];
+
+      const cleanedRegexStringList = configurationRegexStringList.filter(
+        (regexString) => regexString.trim() !== ""
       );
 
-      if (configurationRegex.source === "(?:)") {
+      if (cleanedRegexStringList.length === 0) {
         return defaultRegex;
       }
+
+      const configurationRegexString = cleanedRegexStringList.join("|");
+
+      const configurationRegex = new RegExp(`(?:${configurationRegexString})`);
+
       return configurationRegex;
     } catch (error) {}
+
     return defaultRegex;
   })();
 
