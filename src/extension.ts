bc5756cc10a3754b8a4f851c718c4262dfed5350
Nicolas Descamps <nicolas.descamps@theodo.com>
2024-10-03 17:28:44 +0200

    Add commands

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,66 @@
 import * as vscode from "vscode";
 import { runConfettiTask } from "./tasks/runConfettiTask";
 import { getRegex } from "./settings/getRegex";
+import { sign } from "crypto";
 
 export function activate(context: vscode.ExtensionContext) {
+  // Register the command to enable the extension
+  const enableCommand = vscode.commands.registerCommand(
+    "confetticode.enable",
+    () => {
+      vscode.workspace
+        .getConfiguration("confetticode")
+        .update("enable", true, vscode.ConfigurationTarget.Global);
+      vscode.window.showInformationMessage("ConfettiCode extension enabled!");
+    }
+  );
+
+  // Register the command to disable the extension
+  const disableCommand = vscode.commands.registerCommand(
+    "confetticode.disable",
+    () => {
+      vscode.workspace
+        .getConfiguration("confetticode")
+        .update("enable", false, vscode.ConfigurationTarget.Global);
+      vscode.window.showInformationMessage("ConfettiCode extension disabled!");
+    }
+  );
+
+  // Register the command to add a custom command regex
+  const addCustomCommandRegexCommand = vscode.commands.registerCommand(
+    "confetticode.addCustomCommandRegex",
+    async () => {
+      const customCommandRegex = await vscode.window.showInputBox({
+        placeHolder: "Enter a custom command regex",
+      });
+
+      if (customCommandRegex) {
+        const configurationRegexStringList = vscode.workspace
+          .getConfiguration("confetticode")
+          .get("customCommandRegexList") as string[];
+
+        const updatedConfigurationRegexStringList = [
+          ...configurationRegexStringList,
+          customCommandRegex,
+        ];
+
+        vscode.workspace
+          .getConfiguration("confetticode")
+          .update(
+            "customCommandRegexList",
+            updatedConfigurationRegexStringList,
+            vscode.ConfigurationTarget.Global
+          );
+      }
+    }
+  );
+
+  context.subscriptions.push(
+    enableCommand,
+    disableCommand,
+    addCustomCommandRegexCommand
+  );
+
   vscode.window.onDidEndTerminalShellExecution((event) => {
     // Check if the extension is enabled and exit if it's not
     const isExtensionEnabled = vscode.workspace
